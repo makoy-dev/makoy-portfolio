84002e92870799d20c2404ba7014d64b7dee7cc5
makoy-dev <khristiancamena@gmail.com>
2025-10-08 10:39:12 +0800

    Added project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "@/components/sections/hero";
 import Skills from "@/components/sections/skills";
+import Projects from "@/components/sections/projects";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <div className="pb-[100vh]">
                 <Hero />
                 <Skills />
+                <Projects />
             </div>            
         </>
     );
